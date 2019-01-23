d0019026766dbf6ef184739aa31018a9a6e995b5
陈德祥 <937682896@qq.com>
2019-01-23 13:14:56 +0800

    Initial commit

---
+++ app/src/main/java/com/cdx/example/lastutils/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cdx.example.lastutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cdx.example.lastutils.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
